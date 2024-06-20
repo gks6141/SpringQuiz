f35ad22140a6deb2ab23991732c2c3c568d6b14a
gks6141 <gks6141@hanmail.net>
2024-06-20 15:14:59 +0900

    quiz02

--- src/main/java/com/quiz/lesson04/Lesson04Quiz02Controller.java
+++ src/main/java/com/quiz/lesson04/Lesson04Quiz02Controller.java
@@ -1,15 +1,20 @@
 package com.quiz.lesson04;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.quiz.lesson04.bo.RealtorBO;
+import com.quiz.lesson04.domain.Realtor;
 
 @Controller
 @RequestMapping("/lesson04/quiz02")
 public class Lesson04Quiz02Controller {
+	@Autowired
+	private RealtorBO realtorBO;
 	
 	@GetMapping("/add-realtor-view")
 	public String quiz04() {
@@ -17,21 +22,17 @@ public class Lesson04Quiz02Controller {
 	}
 	
 	@PostMapping("/add-realtor")
-	public String quiz04_1(
-			@RequestParam("office") String office,
-			@RequestParam("phoneNumber") String phoneNumber,
-			@RequestParam("address") String address,
-			@RequestParam("grade") String grade,
-			Model model) {
+	public String quiz04_1(Realtor realtor,Model model) {
 		
 		//insert
-		
+		realtorBO.addRealtor(realtor);
 		//id얻기
 		
 		//select
 		
 		//model
 		model.addAttribute("title","공인중개사 추가");
+		model.addAttribute("result");
 		
 		return "lesson04/addLatestRealtor";
 	}
