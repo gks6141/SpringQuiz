eabc9a6c5f490e8c6a63d63d2cd79f8a4dbb0275
DESKTOP-POO0TJK\user <test@test.com>
2024-06-21 18:55:25 +0900

    타임리프 변수, 반복문

--- src/main/java/com/quiz/lesson05/Lesson05Quiz01Controller.java
+++ src/main/java/com/quiz/lesson05/Lesson05Quiz01Controller.java
@@ -7,15 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/lesson05/quiz01")
 @Controller
-public class Quiz01 {
+public class Lesson05Quiz01Controller {
 	
 	@GetMapping("/1")
 	public String quiz01_1(Model model) {
-		double i = 36;
-		double j = 3;
-		
-		model.addAttribute("i",i);
-		model.addAttribute("j",j);
 		return "lesson05/quiz01";
 	}
 }
