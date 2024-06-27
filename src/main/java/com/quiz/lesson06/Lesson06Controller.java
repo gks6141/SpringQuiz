d6ce8163d74d6b6a8d0fc1ae390c959515ec67ca
gks6141 <gks6141@hanmail.net>
2024-06-27 13:10:22 +0900

    AJAX를 통해서 북마크DB에 insert, select

--- src/main/java/com/quiz/lesson06/Lesson06Controller.java
+++ src/main/java/com/quiz/lesson06/Lesson06Controller.java
@@ -1,29 +1,47 @@
 package com.quiz.lesson06;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.quiz.lesson06.bo.BookmarkBO;
+import com.quiz.lesson06.domain.Bookmark;
 
 @Controller
 @RequestMapping("/lesson06")
 public class Lesson06Controller {
+	@Autowired
+	private BookmarkBO bookmarkBO;
 	
 	@GetMapping	("/add-bookmark-view")
 	public String addBookmarkView() {
 		return "lesson06/addBookmarkView";
 	}
 	
-	
-	@PostMapping("/lesson06/add-bookmark")
-	public String addBookmark() {
+	// AJAX가 하는 요청
+	@ResponseBody
+	@PostMapping("/add-bookmark")
+	public String addBookmark(
+			@RequestParam("name") String name,
+			@RequestParam("url") String url) {
+		bookmarkBO.addBookmark(name, url);
 		return "성공";
 	}
 	
 	
 	
-	@GetMapping("/lesson06/bookmark-list-view")
-	public String bookmarkListView() {
+	@GetMapping("/bookmark-list-view")
+	public String bookmarkListView(Model model) {
+		List<Bookmark> bookmark = bookmarkBO.getBookmark();
+		model.addAttribute("bookmarklist", bookmark);
+		
 		return "lesson06/bookmarkListView";
 	}
 }
