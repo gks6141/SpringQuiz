d9db7be56a6f5263b1b18932862a0618dd1e994a
DESKTOP-POO0TJK\user <test@test.com>
2024-07-01 18:55:06 +0900

    펜션 예약 홈페이지 예약 확인까지 완료

---
+++ src/main/java/com/quiz/booking/mapper/BookingMapper.java
@@ -0,0 +1,28 @@
+package com.quiz.booking.mapper;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.quiz.booking.domain.Booking;
+
+@Mapper
+public interface BookingMapper {
+
+	public List<Booking> selectBookingList();
+	
+	public int deleteBookingById(int id);
+	
+	public void insertBooking(
+			@Param("name") String name, 
+			@Param("date") LocalDate date, 
+			@Param("day") int day, 
+			@Param("headcount") int headcount, 
+			@Param("phoneNumber") String phoneNumber);
+	
+	public List<Booking> selectBookingByName(
+			@Param("name") String name, 
+			@Param("phoneNumber") String phoneNumber);
+}
